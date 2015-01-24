28deba04bec37af2e63abdf94f14989a11166ab5
Ning Deng <ndeng@buffalo.edu>
2015-01-23 19:12:24 -0500

    CSE562 Project0 2nd

--- IdeaProjects/CSE562Proj0/src/edu/buffalo/cse562/Main.java
+++ IdeaProjects/CSE562Proj0/src/edu/buffalo/cse562/Main.java
@@ -4,6 +4,7 @@ package edu.buffalo.cse562;
  * Created by ning on 1/23/15.
  */
 public class Main {
+    //for project0 of CSE562
     public static void main(String args[]){
         String cse="We, the members of our team, agree that we will not submit any code that we have not written ourselves, share our code with anyone outside of our group, or use code that we have not written ourselves as a reference.";
         System.out.println(cse);
